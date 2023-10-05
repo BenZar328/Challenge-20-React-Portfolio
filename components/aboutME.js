3395facb00b76742f53f8efa80270e89a64b1f1b
BenZar328 <129127908+BenZar328@users.noreply.github.com>
2023-10-04 23:36:54 -0400

    Add files via upload

---
+++ components/aboutME.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutMe = () => {
+  return (
+    <section>
+      <h2>About Me</h2>
+      {/* I am a young and excitided candidate learning full stack dev and my photo here */}
+    </section>
+  );
+};
+
+export default AboutMe;
---
+++ components/contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <section>
+      <h2>Contact</h2>
+      <h3>benzar328@gmail.com</h3>
+      <h3>LinkedIn</h3>
+    </section>
+  );
+};
